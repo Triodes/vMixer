d34a3b0e416b37b9040d9fda4f38217652b2a316
Aron List <aronotmar@live.nl>
2015-01-08 21:40:47 +0100

    added forgotten files

---
+++ httpget/Configuration.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace httpget
+{
+    partial class Configuration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cancel = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.defaults = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(546, 287);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // cancel
+            // 
+            this.cancel.Location = new System.Drawing.Point(484, 306);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(75, 23);
+            this.cancel.TabIndex = 1;
+            this.cancel.Text = "Cancel";
+            this.cancel.UseVisualStyleBackColor = true;
+            this.cancel.Click += new System.EventHandler(this.cancel_Click);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(403, 306);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 23);
+            this.save.TabIndex = 2;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // defaults
+            // 
+            this.defaults.Location = new System.Drawing.Point(12, 306);
+            this.defaults.Name = "defaults";
+            this.defaults.Size = new System.Drawing.Size(75, 23);
+            this.defaults.TabIndex = 3;
+            this.defaults.Text = "Defaults";
+            this.defaults.UseVisualStyleBackColor = true;
+            this.defaults.Click += new System.EventHandler(this.defaults_Click);
+            // 
+            // Configuration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 341);
+            this.Controls.Add(this.defaults);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Configuration";
+            this.Text = "Configuration";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button cancel;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button defaults;
+
+
+    }
+}
---
+++ httpget/Configuration.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace httpget
+{
+    public partial class Configuration : Form
+    {
+        public Configuration()
+        {
+            InitializeComponent();
+
+            this.AcceptButton = save;
+
+            LoadData();
+        }
+
+        void LoadData()
+        {
+            dataGridView1.BackgroundColor = Color.White;
+            this.dataGridView1.Columns.Add("Key", "Key");
+            this.dataGridView1.Columns.Add("Value", "Value");
+            dataGridView1.Columns[0].ReadOnly = true;
+            dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            foreach (SettingsProperty e in Settings.Default.Properties)
+            {
+                this.dataGridView1.Rows.Add(e.Name, Settings.Default[e.Name]);
+            }
+            dataGridView1.Sort(dataGridView1.Columns[0], ListSortDirection.Ascending);
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow item in dataGridView1.Rows)
+            {
+                if (item.Cells[0].Value != null)
+                {
+                    string key = item.Cells[0].Value.ToString();
+                    string value = item.Cells[1].Value.ToString();
+                    Settings.Default[key] = value;
+                }
+            }
+            Settings.Default.Save();
+            this.Close();
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void defaults_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Clear();
+            Settings.Default.Reset();
+            LoadData();
+        }
+    }
+}
